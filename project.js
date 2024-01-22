742ebcff2ab1523c84efcd538ed324c917cd831c
JrOyarekhua <Goyarekhua@oglethorpe.edu>
2024-01-21 20:53:09 -0500

    implemented play round function

--- project.js
+++ project.js
@@ -6,4 +6,49 @@ function getComputerChoice() {
   return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+  // compare both selections and return winner
+  let roundWinner;
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === "paper") {
+        roundWinner = "computer";
+      } else if (computerChoice === "scissors") {
+        roundWinner = "player";
+      } else {
+        roundWinner = "tie";
+      }
+      break;
+    case "paper":
+      if (computerChoice === "rock") {
+        roundWinner = "player";
+      } else if (computerChoice === "scissors") {
+        roundWinner = "computer";
+      } else {
+        roundWinner = "tie";
+      }
+      break;
+    case "scissors":
+      if (computerChoice === "paper") {
+        roundWinner = "player";
+      } else if (computerChoice === "rock") {
+        roundWinner = "computer";
+      } else {
+        roundWinner = "tie";
+      }
+      break;
+  }
+
+  if (roundWinner === "player") {
+    return "you won the round";
+  } else if (roundWinner === "computer") {
+    return "you lost the round";
+  } else {
+    return "this round was a tie";
+  }
+}
+
+let playerChoice = prompt("enter rock, paper, or scissors");
+playerChoice = playerChoice.toLowerCase();
+let computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
