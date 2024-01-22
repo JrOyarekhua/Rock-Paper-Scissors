757a09c74a652514bbfa0721bf39cde82662c738
JrOyarekhua <Goyarekhua@oglethorpe.edu>
2024-01-21 21:24:59 -0500

    completed game

--- project.js
+++ project.js
@@ -48,7 +48,50 @@ function playRound(playerChoice, computerChoice) {
   }
 }
 
-let playerChoice = prompt("enter rock, paper, or scissors");
-playerChoice = playerChoice.toLowerCase();
-let computerChoice = getComputerChoice();
-console.log(playRound(playerChoice, computerChoice));
+// let playerChoice = prompt("enter rock, paper, or scissors");
+// playerChoice = playerChoice.toLowerCase();
+// let computerChoice = getComputerChoice();
+// // console logs
+// console.log(`you chose ${playerChoice}`);
+// console.log(`the computer chose ${computerChoice}`);
+// console.log(playRound(playerChoice, computerChoice));
+
+function playGame() {
+  // play 5 rounds and log each round
+  // who ever wins the most rounds wins
+  let computerWins = 0;
+  let userWins = 0;
+  let ties = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt("enter rock, paper, or scissors");
+    playerChoice = playerChoice.toLowerCase();
+    console.log(`you chose ${playerChoice}`);
+    let computerChoice = getComputerChoice();
+    console.log(`the computer chose ${computerChoice}`);
+    let winner = playRound(playerChoice, computerChoice);
+    console.log(winner);
+    // track wins
+    switch (winner) {
+      case "you won the round":
+        userWins += 1;
+        break;
+      case "you lost the round":
+        computerWins += 1;
+        break;
+      case "this round was a tie":
+        ties += 1;
+        break;
+    }
+  }
+
+  if (computerWins > userWins) {
+    console.log("you lost the game");
+  } else if (computerWins < userWins) {
+    console.log("you won the game !");
+  } else {
+    console.log("The whole game was a tie");
+  }
+}
+
+playGame();
